c9d6b1f9309494ce13371a8f773c720f6639fe8b
umang8836 <83946178+umang8836@users.noreply.github.com>
2021-12-09 16:20:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ScriptTag from 'react-script-tag';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ScriptTag type="text/javascript" src="sketch.js" />
+    </div>
+  );
+}
+
+export default App;
